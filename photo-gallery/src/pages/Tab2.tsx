c475ec672b7b0c6fcead8946ed74f6b820f4fe59
lfbook0831 <116310068+lfbook0831@users.noreply.github.com>
2023-12-03 22:47:00 +0000

    Initial commit

---
+++ photo-gallery/src/pages/Tab2.tsx
@@ -0,0 +1,47 @@
+import { usePhotoGallery } from '../hooks/usePhotoGallery';
+import { camera } from 'ionicons/icons';
+  import {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonActionSheet,
+  } from '@ionic/react';
+  import ExploreContainer from '../components/ExploreContainer';
+  import './Tab2.css';
+
+  const Tab2: React.FC = () => {
+    const { takePhoto } = usePhotoGallery();
+    return (
+<IonPage>
+  <IonHeader>
+    <IonToolbar>
+      <IonTitle>Luisa Photo Gallery</IonTitle>
+    </IonToolbar>
+  </IonHeader>
+  <IonContent fullscreen>
+    <IonHeader collapse="condense">
+      <IonToolbar>
+        <IonTitle size="large">Tab 2</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <ExploreContainer name="Tab 2 page" />
+    <IonFab vertical="bottom" horizontal="center" slot="fixed">
+      <IonFabButton onClick={() => takePhoto()}>
+        <IonIcon icon={camera}></IonIcon>
+      </IonFabButton>
+    </IonFab>
+  </IonContent>
+</IonPage>
+);
+};
+
+export default Tab2;
